665631f91b6abd7186485151c0f3b521c5cc838e
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 18:01:01 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.java
@@ -0,0 +1,25 @@
+// Last updated: 8/4/2025, 6:01:04 PM
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        calc(numRows, 0, 0, 1, result, ans);
+        return result;
+
+    }
+
+    public static void calc(int r, int row, int col, int ncr, List<List<Integer>> result, List<Integer> ans) {
+        if (row == r) {
+            return;
+        }
+        if (col > row) {
+            result.add(new ArrayList<>(ans));
+            calc(r, row + 1, 0, 1, result,new ArrayList<>());
+            return;
+        }
+        ans.add(ncr);
+        int nextNcr = ncr * (row - col) / (col + 1);
+        calc(r, row, col + 1, nextNcr, result,ans);
+
+    }
+}
