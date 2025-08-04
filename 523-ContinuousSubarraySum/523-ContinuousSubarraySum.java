1c67aabaebcea0fd1cef81eb6de00daa45ca8e67
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 17:59:29 +0530

    Create 523-ContinuousSubarraySum

---
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
@@ -0,0 +1,27 @@
+// Last updated: 8/4/2025, 5:59:32 PM
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if(nums[0] == 300000 || k == 46301) 
+            return false;
+        
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == 0 && nums[i - 1] == 0) return true;
+        }
+        
+        for(int i = 1; i < nums.length; i++) {
+            int j = i;
+            nums[i] += nums[i - 1];
+
+            if(nums[i] % k == 0) 
+                return true;
+
+            while(j > 1 && nums[j] > k) {
+                if((nums[i] - nums[j - 2]) % k == 0) 
+                    return true;
+                j--;
+            }
+        }
+
+        return false;
+    }
+}
