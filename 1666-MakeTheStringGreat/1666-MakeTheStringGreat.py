4f44abd974eaafa4b5252a50cf0f1e8ebfb0212b
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 17:58:07 +0530

    Create 1666-MakeTheStringGreat

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.py
@@ -0,0 +1,12 @@
+# Last updated: 8/4/2025, 5:58:10 PM
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and abs(ord(stack[-1]) - ord(char)) == 32:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
+            
+
