fc36a186d0d6ba8ef7584afd49bb5011be81be06
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 17:59:17 +0530

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -0,0 +1,21 @@
+// Last updated: 8/4/2025, 5:59:20 PM
+import java.util.Arrays;
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (nums[right] - nums[left] > 1) {
+                left++;
+            }
+
+            if (nums[right] - nums[left] == 1) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+
+        return maxLen;
+    }
+}
