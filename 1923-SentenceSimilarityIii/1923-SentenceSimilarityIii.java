4b6e1d6a5c0d4e114cdbee69563653a21f9540cf
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 17:57:54 +0530

    Create 1923-SentenceSimilarityIii

---
+++ 1923-SentenceSimilarityIii/1923-SentenceSimilarityIii.java
@@ -0,0 +1,20 @@
+// Last updated: 8/4/2025, 5:57:57 PM
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+        
+        int i = 0;
+        while (i < s1.length && i < s2.length && s1[i].equals(s2[i])) {
+            i++;
+        }
+        
+        int j = 0;
+        while (j < s1.length - i && j < s2.length - i &&
+               s1[s1.length - 1 - j].equals(s2[s2.length - 1 - j])) {
+            j++;
+        }
+        
+        return i + j == Math.min(s1.length, s2.length);
+    }
+}
