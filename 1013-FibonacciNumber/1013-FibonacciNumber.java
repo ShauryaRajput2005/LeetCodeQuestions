13372d587d3600450b50d8dcd9c6a873cffa503f
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-04 17:58:39 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2025, 5:58:42 PM
+class Solution {
+    public int fib(int n) {
+        return fibonacci(n);
+    }
+    public static int fibonacci(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        int f1=fibonacci(n-1);
+        int f2=fibonacci(n-2);
+        return f1+f2;
+    }
+}
