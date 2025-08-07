9cf6a41740e63dfdaa3f5a446c5f661f397f64e2
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-07 11:07:42 +0530

    Create 554-BrickWall

---
+++ 554-BrickWall/554-BrickWall.java
@@ -0,0 +1,20 @@
+// Last updated: 8/7/2025, 11:07:45 AM
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Long, Integer> map=new HashMap<>();
+        for(List<Integer> rows : wall){
+            long sum=0;
+            for(int i=0;i<rows.size()-1;i++){
+                sum+=rows.get(i);
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+        int maxEdges = 0;
+        for (int count : map.values()) {
+            maxEdges = Math.max(maxEdges, count);
+        }
+
+        return wall.size() - maxEdges;
+
+    }
+}
