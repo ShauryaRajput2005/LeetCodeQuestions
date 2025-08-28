0c9673ffb062e86299bbba922b632b4c17c2f5f5
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-28 21:52:58 +0530

    Create 1448-Maximum69Number

---
+++ 1448-Maximum69Number/1448-Maximum69Number.java
@@ -0,0 +1,13 @@
+// Last updated: 8/28/2025, 9:52:56 PM
+class Solution {
+    public int maximum69Number (int num) {
+        char[] s = String.valueOf(num).toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(s));
+    }
+}
