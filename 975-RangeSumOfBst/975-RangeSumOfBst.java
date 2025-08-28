40a05237e3d86aaf2bfc1cbfb5e7fedd6e3500e1
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-28 21:53:13 +0530

    Create 975-RangeSumOfBst

---
+++ 975-RangeSumOfBst/975-RangeSumOfBst.java
@@ -0,0 +1,34 @@
+// Last updated: 8/28/2025, 9:53:12 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+        if (root.left != null && root.val >= low) {
+            sum += rangeSumBST(root.left, low, high);
+        }
+        if (root.right != null && root.val <= high) {
+            sum += rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+}
