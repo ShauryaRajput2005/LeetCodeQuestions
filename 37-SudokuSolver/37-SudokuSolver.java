8609eb3c673d61310dc9644815ffabf280c5592b
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-31 16:53:08 +0530

    File updated at 8/31/2025, 4:53:08 PM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,63 +1,54 @@
-// Last updated: 8/4/2025, 6:01:54 PM
+// Last updated: 8/31/2025, 4:53:08 PM
 class Solution {
-
     public void solveSudoku(char[][] board) {
-        solver(board, 0, 0);
+        solver(0, 0, board);        
     }
 
-    public boolean isSafe(char[][] board, int row, int col, int num) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == (char) (num + '0')) {
-                return false;
-            }
-        }
+    public static boolean solver(int row, int col, char[][] board) {
+        if(row == 9) return true;
 
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == (char) (num + '0')) {
-                return false;
-            }
-        }
-
-        int sr = 3 * (row / 3);
-        int sc = 3 * (col / 3);
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (board[i][j] == (char) (num + '0')) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public boolean solver(char[][] board, int row, int col) {
-        if (row == board.length) {
-            return true;
-        }
-
-        int nrow = row, ncol = col + 1;
-        if (col == board.length - 1) {
+        int nrow = 0, ncol = 0;
+        if(col == 8) {
             nrow = row + 1;
             ncol = 0;
+        } else {
+            nrow = row;
+            ncol = col + 1;
         }
 
-        if (board[row][col] != '.') {
-            return solver(board, nrow, ncol);
+        if(board[row][col] != '.') {
+            return solver(nrow, ncol, board);
         } else {
-            for (int i = 1; i <= 9; i++) {
-                if (isSafe(board, row, col, i)) {
-                    board[row][col] = (char) (i + '0');
-
-                    if (solver(board, nrow, ncol)) {
-                        return true;
-                    } else {
-                        board[row][col] = '.';
-                    }
+            for(int i = 1; i <= 9; i++) {
+                if(isSafe(board, row, col, i)) {
+                    board[row][col] = (char)(i + '0');
+                    if(solver(nrow, ncol, board)) return true;
+                    board[row][col] = '.';
                 }
             }
         }
-
         return false;
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col, int num) {
+        char n = (char)(num + '0');
+
+        for(int i=0; i<9; i++) {
+            if(board[row][i] == n) return false;
+        }
+
+        for(int i=0; i<9; i++) {
+            if(board[i][col] == n) return false;
+        }
+
+        int cr = 3 * (row/3);
+        int cc = 3 * (col/3);
+
+        for(int i=cr; i<cr+3; i++) {
+            for(int j=cc; j<cc+3; j++) {
+                if(board[i][j] == n) return false;
+            }
+        }
+        return true;
     }
 }
