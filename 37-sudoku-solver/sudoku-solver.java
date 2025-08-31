2f6e74845a6b9d068c76f2df65505444546fb3e1
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-08-31 16:53:11 +0530

    Time: 184 ms (74.90%) | Memory: 41.3 MB (43.93%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solver(0, 0, board);        
+    }
+
+    public static boolean solver(int row, int col, char[][] board) {
+        if(row == 9) return true;
+
+        int nrow = 0, ncol = 0;
+        if(col == 8) {
+            nrow = row + 1;
+            ncol = 0;
+        } else {
+            nrow = row;
+            ncol = col + 1;
+        }
+
+        if(board[row][col] != '.') {
+            return solver(nrow, ncol, board);
+        } else {
+            for(int i = 1; i <= 9; i++) {
+                if(isSafe(board, row, col, i)) {
+                    board[row][col] = (char)(i + '0');
+                    if(solver(nrow, ncol, board)) return true;
+                    board[row][col] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col, int num) {
+        char n = (char)(num + '0');
+
+        for(int i=0; i<9; i++) {
+            if(board[row][i] == n) return false;
+        }
+
+        for(int i=0; i<9; i++) {
+            if(board[i][col] == n) return false;
+        }
+
+        int cr = 3 * (row/3);
+        int cc = 3 * (col/3);
+
+        for(int i=cr; i<cr+3; i++) {
+            for(int j=cc; j<cc+3; j++) {
+                if(board[i][j] == n) return false;
+            }
+        }
+        return true;
+    }
+}
