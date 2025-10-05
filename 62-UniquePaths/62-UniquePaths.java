fd2079dd387b87649b0ddedeb1ddefcace055f0b
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2025-10-05 22:53:17 +0530

    File updated at 10/5/2025, 10:53:17 PM

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,18 +1,18 @@
-// Last updated: 8/4/2025, 6:01:30 PM
+// Last updated: 10/5/2025, 10:53:17 PM
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
 
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1; // First column has only one way
+            dp[i][0] = 1; 
         }
         for (int j = 0; j < n; j++) {
-            dp[0][j] = 1; // First row has only one way
+            dp[0][j] = 1; 
         }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // Sum of top and left cell
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; 
             }
         }
         return dp[m - 1][n - 1];
