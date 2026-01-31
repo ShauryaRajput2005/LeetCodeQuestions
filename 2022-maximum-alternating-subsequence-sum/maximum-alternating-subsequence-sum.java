79ad3549d68a7f04922224be56f758bbe150451f
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2026-01-31 23:12:57 +0530

    Time: 29 ms (67.24%) | Memory: 104.4 MB (64.58%) - LeetSync

---
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n=nums.length;
+        long[][] t=new long[n+1][2];
+
+        for(int i=1;i<n+1;i++){
+            //even
+            t[i][0]=Math.max(t[i-1][1]+nums[i-1],t[i-1][0]);
+
+            //odd
+            t[i][1]=Math.max(t[i-1][0]-nums[i-1],t[i-1][1]);
+        }        
+    return Math.max(t[n][0],t[n][1]);
+    }
+}
