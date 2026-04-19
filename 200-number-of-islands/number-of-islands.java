6dcc161ba09d29bf4d33bebe70a07a8b6f186ef1
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2026-04-20 05:13:35 +0530

    Time: 5 ms (27.88%) | Memory: 51 MB (97.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,35 @@
 class Solution {
     public int numIslands(char[][] grid) {
-     int cnt = 0;
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] == '1') {
-                        sink(grid, i, j);
-                        cnt++;
-                    }
+        int n=grid.length;
+        int m=grid[0].length;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1') {
+                    bfs(grid,i,j);
+                    cnt++;
                 }
             }
-            return cnt;
         }
-
-        public static void sink(char[][] grid, int row, int col) {
-            if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
-                return;
-            }
-            grid[row][col] = '0';
-            sink(grid, row + 1, col);
-            sink(grid, row - 1, col);
-            sink(grid, row, col + 1);
-            sink(grid, row, col - 1);
-        }
+    return cnt;
     }
 
+    public static void bfs(char[][] grid,int r,int c){
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{r,c});
+        int[][] dir={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!q.isEmpty()){
+            int[] cur=q.poll();
+            for(int[] d:dir){
+                int nr=d[0]+cur[0];
+                int nc=d[1]+cur[1];
+                if(nr>=0 && nc>=0 && nc<grid[0].length && nr<grid.length && grid[nr][nc]=='1'){
+                    grid[nr][nc]='0';
+                    q.offer(new int[]{nr,nc});
+                }
+            }
+
+        } 
+    }
+}
   
