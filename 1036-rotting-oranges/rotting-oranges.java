86d7f79530237ab3cad20b8c63ce832c97e209ff
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2026-08-07 15:45:29 +0530

    Time: 2 ms (89.93%) | Memory: 44.1 MB (56.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,38 +1,43 @@
 class Solution {
-
     public int orangesRotting(int[][] grid) {
-        int fr=0;
-        Queue<int[]> q = new LinkedList();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
+        int cnt=0;
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q=new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(grid[i][j]==2){
                     q.offer(new int[]{i,j});
                 }
-                if(grid[i][j]==1) fr++;
-            }
-        }
-    
-        int[][] dir = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-             boolean rotting = false;
-            while (size-- > 0) {
-                int[] cur = q.poll();
-                for (int[] d : dir) {
-                    int nr = d[0] + cur[0];
-                    int nc = d[1] + cur[1];
-                    if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        fr--;
-                        q.offer(new int[] { nr, nc });
-                        rotting=true;
-                    }
+                if(grid[i][j]==1){
+                    cnt++;
                 }
             }
-            if(rotting) cnt++;
-
         }
-        return (fr>0) ? -1 :cnt; 
+        int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
+        int t=0;
+        boolean rot=false;
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            rot=false;
+            while(size-->0){
+                int[] cur=q.poll();
+                for(int i=0;i<4;i++){
+                    int nr=cur[0] + dir[i][0];
+                    int nc=cur[1] + dir[i][1];
+                    if(nr<0 || nc<0 || nr>=n || nc>=m ){
+                        continue;
+                    }
+                    if(grid[nr][nc]!=1) continue;
+                    grid[nr][nc]=2;
+                    rot=true;
+                    cnt--;
+                    q.offer(new int[]{nr,nc});
+                }
+            }
+            if(rot) t++;
+        }
+        return (cnt==0)? t:-1;
     }
 }
